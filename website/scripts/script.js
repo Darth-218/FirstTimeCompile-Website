e11ec8d495a19e1b29804910e9d9f960474a4669
alchemistsGestalt <zeinhatem@proton.me>
2023-10-28 11:51:08 +0200

    Created a JS script to automatically add navigation links so we don't have to add them manually to each page. This is a lot better, in my opinion, than the iframes method I demonstrated in the `frames` branch. The code should be clearly commented so it shouldn't be too hard to understand.

---
+++ website/scripts/script.js
@@ -0,0 +1,26 @@
+const navigation = document.querySelector('nav');
+
+/* list of links we want in the navigation bar */
+const links = {
+  "Home": "index.html",
+  "About Us": "about.html",
+};
+
+let i = 0;
+for (const link in links) {
+  /* creates a <a> element */
+  const link_elem = document.createElement('a');
+  /* sets the text the link will have */
+  link_elem.textContent = link;
+  /* sets the href attribute to be the html file the link will go to */
+  link_elem.setAttribute('href', links[link]);
+
+  /* adds a separator between links, but not at the end or at the start */
+  if ((i != links.legth - 1) && (i != 0)) {
+    const separator = document.createTextNode(" — ")
+    navigation.appendChild(separator);
+  }
+
+  navigation.appendChild(link_elem);
+  ++i;
+}
