54b3546c3aa5672e882fd4ac26d8b423a191b88b
alchemistsGestalt <zeinhatem@proton.me>
2023-11-05 11:03:39 +0200

    Did some stuff.
    Added youssef's two pages (still gonna work on them, currently they only have their text). Changed the JavaScript to work with Afifi's navigation bar setup. Changed his css a bit to make it work with longer bars etc.  We now need to incorporate everyone's pages into this system.

--- website/scripts/nav_bar_script.js
+++ website/scripts/nav_bar_script.js
@@ -1,31 +1,40 @@
-const navigation = document.querySelector('nav');
+const navigation = document.querySelector('div');
 
 /* list of links we want in the navigation bar */
 const links = {
   "Home": "index.html",
+  "Introduction": "ahmed/afifi's_page1.html",
+  "Developement": "yahia/Development of programming languages.html",
   "Types": "types.html",
   "Preparation": "preparation.html",
   "Terminology": "terms.html",
+  "What to Learn?": "youssef1.html",
   "About Us": "about.html",
 };
 
 let i = 0;
 for (const link in links) {
+  const div_wrapper = document.createElement('div');
+  div_wrapper.setAttribute('class', 'button');
+
   /* creates an <a> element */
   const link_elem = document.createElement('a');
   /* sets the text the link will have */
   link_elem.textContent = link;
   /* sets the href attribute to be the html file the link will go to */
   link_elem.setAttribute('href', links[link]);
-  link_elem.setAttribute('class', 'nav-item');
+  link_elem.setAttribute('target', '_top');
 
   /* adds a separator between links, but not at the end or at the start */
   if ((i != links.legth - 1) && (i != 0)) {
-    const separator = document.createTextNode(" — ")
+    // const separator = document.createTextNode(" — ")
+    const separator = document.createElement('span')
+    separator.setAttribute('class', 'verticalSerparator');
     navigation.appendChild(separator);
   }
 
   /* adds the final <a> element to the <nav> tag */
-  navigation.appendChild(link_elem);
+  div_wrapper.appendChild(link_elem);
+  navigation.appendChild(div_wrapper);
   ++i;
 }
