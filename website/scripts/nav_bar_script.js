9e60461805f8332f79a11b5f755329f7296d2783
alchemistsGestalt <zeinhatem@proton.me>
2023-11-05 19:34:47 +0200

    Updated mine and Youssef's pages.

--- website/scripts/nav_bar_script.js
+++ website/scripts/nav_bar_script.js
@@ -14,28 +14,30 @@ const links = {
 
 let i = 0;
 for (const link in links) {
+  /* Create a `div` element that will contain the link */
   const div_wrapper = document.createElement('div');
+  /* Class for css */
   div_wrapper.setAttribute('class', 'button');
 
-  /* creates an <a> element */
+  /* Create an `a` element */
   const link_elem = document.createElement('a');
-  /* sets the text the link will have */
+  /* Set the text the link will have */
   link_elem.textContent = link;
   /* sets the href attribute to be the html file the link will go to */
   link_elem.setAttribute('href', links[link]);
+
+  /* Because the navigation bar is in an `iframe`, we have to set the target
+   * to `_top` so that it switches the whole page and not just the frame
+   */
   link_elem.setAttribute('target', '_top');
 
   /* adds a separator between links, but not at the end or at the start */
   if ((i != links.legth - 1) && (i != 0)) {
-    // const separator = document.createTextNode(" — ")
     const separator = document.createElement('span')
     separator.setAttribute('class', 'verticalSerparator');
     navigation.appendChild(separator);
   }
 
-  /* adds the final <a> element to the <nav> tag */
-  // div_wrapper.appendChild(link_elem);
-  // navigation.appendChild(div_wrapper);
   navigation.appendChild(link_elem);
   ++i;
 }
